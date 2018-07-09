719fbd09000868c3b0631eaaaa7ee9d77e75dc61
ava krishn <avakrishn@gmail.com>
2018-07-08 17:50:40 -0700

    added pseudocode comments to explain code; add functions where when mouseover gif the rating is displayed on gif but when mouse leaves the gif or if the gif is clicked to play/pause the gif then the rating is hidden

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,23 @@
+//global variables
 var movies = ["Back To The Future", "Solo: A Star Wars Story", "Avengers Infinity War", "The Sound Of Music", "Zootopia"];
 var musicians = ["OK Go", "Bruno Mars", "Imagine Dragons", "Adele", "Green Day"];
-var sportsTeams = ["Golden State Warriors", "New England Patriots", "San Francisco Giants", "Toronto Maple Leafs", "LA Galaxy"];
+var sportsTeams = ["Golden State Warriors", "Philadelphia Eagles", "San Francisco Giants", "Toronto Maple Leafs", "LA Galaxy"];
 
 
 
 
 function addButtons(){
-    event.preventDefault();
     var userInput = $(this).prev().val().trim().toLowerCase(); // the prev method gets the immediate predecessor of the element in the DOM tree, in this case $(this) is the specific form button pressed and the .prev() method finds the input field that was just above the button pressed. (.prev() is similar to .next())
     if(userInput !== ""){
         if($(this).parent().attr('id') == "movieForm") //.parent() method gets the parent div that the element is nested in, in the DOM tree.
         {
             for(var i in movies){
-                if (movies[i].toLowerCase() === userInput){
+                if (movies[i].toLowerCase() === userInput){ //toLowerCase will make it easier to compare if movies array already has the movie that the user entered
                     $(this).prev().val("");
-                    return false;
+                    return false; // will return false and will stop the progression through function if movies array already has the movie that the user entered
                 }   
             }
+            // new movie button is created for the movie the user entered (not calling the createButtons function to iterate over all values in the array inorder to preserve the offset number that is an attribute of each button)
             movies.push(userInput);
             var movieButton = $("<button>").addClass("btn btn-primary btn-sm m-1 movie").html(userInput);
             movieButton.attr('data-offset', 0);
@@ -49,7 +50,7 @@ function addButtons(){
             $('#sportsTeamButtons').append(sportsTeamButton);
         }
     }
-    $(this).prev().val("");
+    $(this).prev().val(""); //clears the input text field
 }
 
 
@@ -57,30 +58,31 @@ function addGifs (){
     var offset = $(this).attr('data-offset'); //keeps track of current value of data-offset of the button that was pressed
     var selection = $(this).text();
     console.log(selection);
-    var gifURL = "https://api.giphy.com/v1/gifs/search?q="+selection+"&offset="+offset+"&api_key=bq07u6AXsHP4sCiu6fbhkT6IVvpQsfbS&limit=10"
-    var gifDiv = $(this).parent().next()
+    var gifURL = "https://api.giphy.com/v1/gifs/search?q="+selection+"&offset="+offset+"&api_key=bq07u6AXsHP4sCiu6fbhkT6IVvpQsfbS&limit=10" //offset will offset the gifs returned by a certain number; gifs are limited to 10 for each query
+    var gifDiv = $(this).parent().next() //depending on if in movie, musician, or sports Team sections, $(this).parent will referece the respective movieButtons, musicianButtons, or sportsTeamButtons div and .next() will reference the respective the movieGifs, musicianGifs, or sportsTeamGifs div
     gifDiv.empty(); // empties the Gifs div under the button div that is in respect to the button pressed
-    var infoDiv = $(this).parent().next().next()
+    var infoDiv = $(this).parent().next().next() //reference the respective Info div depending on if in movie, musician, or sport Teams section
     infoDiv.empty(); //empties the Info div under the Gifs div that is in respect to the button pressed
 
-
+    // Querying GIPHY API with request
     $.ajax({
         url: gifURL,
         method: "GET"
-    }).then(function(response){
-        console.log(response);
+    }).then(function(response){ //response is data in the form of JSON object recieved from API
+        // console.log(response);
         for (var i in response.data){
             // var title = $("<h4>").html("Title: "+ response.data[i].title);
-            var newDiv = $("<div>").addClass("float-left m-2");
-            var newGif = $("<img>").addClass('gif').attr("src", response.data[i].images.fixed_height_small_still.url);
-            newGif.attr({
-                "data-still": response.data[i].images.fixed_height_small_still.url,
-                "data-animate": response.data[i].images.fixed_height_small.url,
+            var newDiv = $("<div>").addClass("gifDiv m-1");
+            var newGif = $("<img>").addClass('gif').attr("src", response.data[i].images.fixed_height_still.url); //using object keys to select specific value in JSON object
+            newGif.attr({ //creating new data attributes to keep track of animate or still state that will be used in another function to change the state based on when the user clicks on the div
+                "data-still": response.data[i].images.fixed_height_still.url,
+                "data-animate": response.data[i].images.fixed_height.url,
                 "data-state": "still",
             });
-            var rating = $("<h5>").html("Rating: " + response.data[i].rating);
-            newDiv.append(newGif, rating);
-            gifDiv.append(newDiv);
+            var rating = $("<h5>").html("Rating: " + response.data[i].rating); //rating for each gif
+            rating.addClass("gifRating");
+            newDiv.append(newGif, rating); //appends gif and rating to div
+            gifDiv.append(newDiv); // appends the div to the page
         }
     });
     $(this).attr('data-offset', parseInt(offset)+10); //sets the data-offset value equal to the original data-offset value + 4 that is unique to the specific button
@@ -89,17 +91,19 @@ function addGifs (){
 
 
 function playPauseGifs(){
-    if($(this).attr("data-state") == "still"){
+    $('.gifRating').hide(); //hide the rating when the user clicks the gif
+    
+    if($(this).attr("data-state") == "still"){ //if the current state of gif is still on user click then changes state to animate by changing image src
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate"); 
     }
-    else{
+    else{ //if the current state of gif is animate on user click then changes state to still by changing image src
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
     }
 }
 
-function createButtons(){
+function createButtons(){ //creates buttons that are in the respective arrays and is called on page load
     for(var i in movies){
         var movieButton = $("<button>").addClass("btn btn-primary btn-sm m-1 movie").html(movies[i]);
         movieButton.attr('data-offset', 0); // gives the button a unique data attribute, 'data-offset' whose value will increase by 4 every time the button is pressed and the value will be used when querying the API inorder to display new gifs to user each time the button is pressed.
@@ -120,11 +124,21 @@ function createButtons(){
 }
 
 
-$(window).ready(function(){
+$(window).ready(function(){ //when the window is ready create buttons from the respective global variable arrays and on button (inside the form) click, execute addButtons function
     createButtons();
     $('form button').on('click', addButtons);
+    
 });
 
 
-$(document).on('click', '.gif', playPauseGifs);
-$(document).on('click', '.movie, .musician, .sportsTeam', addGifs);
+$(document).on('click', '.gif', playPauseGifs); //executes the playPauseGifs function when .gif is clicked
+$(document).on('click', '.movie, .musician, .sportsTeam', addGifs); //executes the addGifs function when either .movie, .musician, or .sportsTeam is clicked
+$(document).on('mouseenter','.gifDiv', function(){ //when mouse enters the div containing the gif then the rating will be diplayed
+    $('.gifRating').show();
+
+});
+$(document).on('mouseleave','.gifDiv', function(){ // when mouse leavs the div containing the gif then the rating will be hidden
+    $('.gifRating').hide();
+
+});
+
